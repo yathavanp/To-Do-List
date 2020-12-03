8aafcc361a6000daa0d2d0d50c42e22d14615b23
Yathavan Push <pushyathavan@gmail.com>
2020-12-03 15:18:09 -0500

    final files

--- app.js
+++ app.js
@@ -161,6 +161,10 @@ app.post("/reset", function (req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function () {
-  console.log("Server running on Port:3000 ...");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function () {
+  console.log("Server running on Port:" + port + "...");
 });
